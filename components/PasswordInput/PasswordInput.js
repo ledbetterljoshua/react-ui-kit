f4e88db76499176d921f2317c93cba7ce8cfb0d6
ledbetterljoshua <ledbetterljoshua@gmail.com>
2018-04-26 11:37:53 -0500

    using the component creator to create a component :)

---
+++ components/PasswordInput/PasswordInput.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components'
+
+function PasswordInput() {
+  return (
+    <div>
+    </div>
+  )
+}
+
+PasswordInput.propTypes = {
+};
+
+export default PasswordInput;
---
+++ stories/examples/PasswordInput/PasswordInput.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+import { PasswordInput } from '../../../components' //'grid-ui'
+
+const PasswordInputExample = ({...props}) => {
+  return (
+    <PasswordInput {...props} />
+  )
+}
+
+export default PasswordInputExample
